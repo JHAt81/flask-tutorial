16f90e75891c63b1ae17b9dd5a63408c9a7f81b4
Tanaka Jhamba <tnkjham@gmail.com>
2024-09-17 02:59:11 +0200

    added login view to auth blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,9 +5,10 @@ from flask import Flask
 def create_app(test_config=None):
     #CREATE AND CONFIGURE APP
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY='dev',
-                            DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-                            )
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
     
     if test_config is None:
         #LOAD THE INSTANCE CONFIG, IF IT EXISTS, WHEN NOT TESTING
@@ -26,5 +27,12 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     
     return app
